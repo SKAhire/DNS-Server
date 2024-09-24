646694013b7b6133e1a44dab9b7fe70ccbfffb71
Guru <shubhamka13@gmail.com>
2024-09-24 22:07:33 +0530

    first commit

---
+++ Node Model/Stage1/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className='text-center bg-red-300'>
+      <h1>Welcome to My React App with TypeScript!</h1>
+    </div>
+  );
+}
+
+export default App;
