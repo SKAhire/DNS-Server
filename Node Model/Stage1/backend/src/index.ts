5d28e96d271a396be4506c5ffb2e5e8be8198fa2
Guru <shubhamka13@gmail.com>
2024-10-02 19:22:44 +0530

    setup udp server using dgram(already in node js)

--- Node Model/Stage1/backend/src/index.ts
+++ Node Model/Stage1/backend/src/index.ts
@@ -1,6 +1,29 @@
-const sayHello = (name: string) => {
-    console.log(`Hello, ${name}`);
-  };
-  
-  sayHello('Guru');
-  
+import * as dgram from 'dgram'; // Correct import for TypeScript
+const server: dgram.Socket = dgram.createSocket('udp4');
+
+server.on('error', (err: Error) => {
+  console.error(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg: Buffer, rinfo: dgram.RemoteInfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(53, () => console.log("DNS is on port 53"));
+// Prints: server listening 0.0.0.0:53
+
+
+// use this to check if your udp is working properly or not(if you don't have telnet, netcat, etc)
+// just paste this lines as it is in powershell make sure the check if you are using correct port(port can be any thing my is 53)
+// $udpClient = New-Object System.Net.Sockets.UdpClient
+// $serverAddress = [System.Net.IPAddress]::Parse("127.0.0.1")
+// $remoteEndPoint = New-Object System.Net.IPEndPoint($serverAddress, 53)
+// $message = [System.Text.Encoding]::ASCII.GetBytes("Hello from PowerShell")
+// $udpClient.Send($message, $message.Length, $remoteEndPoint)
+// $udpClient.Close()
